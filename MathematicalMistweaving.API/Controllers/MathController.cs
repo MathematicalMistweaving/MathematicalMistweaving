6a7fc57e8d38c3e29f7e9534316d7ba10e63f6cc
vohrr <matthew.d.pingel@gmail.com>
2023-07-15 14:54:10 -0400

    add Math Controller with blank endpoint

---
+++ MathematicalMistweaving.API/Controllers/MathController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mistweaver.SpellData;
+using Mistweaver.SpellData.Helpers;
+using Mistweaver.SpellData.Interfaces;
+using System.Runtime.Caching;
+
+namespace MathematicalMistweaving.API.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{verison:apiVersion}/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        private readonly ISpellBook spellBook;
+
+        public MathController(ISpellBook spellBook)
+        {
+            this.spellBook = spellBook;
+        }
+
+        [HttpGet("")]
+        public ActionResult<HealBase> GetModifiedSpell()
+        {
+            return NotFound();
+        }
+    }
+
+}
