a14f10ab523b1be8756a3842618de481855b9579
vohrr <matthew.d.pingel@gmail.com>
2023-07-31 14:45:50 -0400

    random files that will probably get deleted

---
+++ mathematicalmistweaving.web/src/common/simc.ts
@@ -0,0 +1,170 @@
+import React from 'react';
+
+/**# Vohrr - Mistweaver - 2023 - 07 - 30 11: 10 - US / Sargeras
+# SimC Addon 10.1.5 - 02
+# WoW 10.1.5.50622, TOC 100105
+# Requires SimulationCraft 1000 - 01 or newer
+
+monk = "Vohrr"
+level = 70
+race = night_elf
+region = us
+server = sargeras
+role = attack
+professions = mining = 100 / jewelcrafting=100
+spec = mistweaver
+
+talents = B4QAa92LNqySTL5b6fvViReva0CFkIRpoRIlQSKRJRatAAAAAAAAAAAAASaSahEJJaFQLJphkDAAgA
+
+# Saved Loadout: M +
+# talents = B4QAa92LNqySTL5b6fvViReva0SShkEiSrFhUiIpBpkiAAAAAAAAAAAAAkmkSSSkkoVAtkkGEOAAgI
+# Saved Loadout: ChijiAT
+# talents = B4QAa92LNqySTL5b6fvViReva0CFkIhSrQiISSahkWKCAAAAAAAAAAAAASaRSkkoVQ0SSaEJAAA
+# Saved Loadout: Raid Mana
+# talents = B4QAa92LNqySTL5b6fvViReva0CFkIhSrJhISSKhkWaCAAAAAAAAAAAAASKhEJJaFQLJplI5AAAIA
+# Saved Loadout: Raid LC FT
+# talents = B4QAa92LNqySTL5b6fvViReva0CFkIRpoRIlQSKRJRatAAAAAAAAAAAAASaSahEJJaFQLJphkDAAgA
+# Saved Loadout: Alternate M +
+# talents = B4QAa92LNqySTL5b6fvViReva0iUKSiEtSrRIlISKQKpJAAAAAAAAAAAAApJpkkEJJaF4AtkkGEAAIC
+# Saved Loadout: Rashok
+# talents = B4QAa92LNqySTL5b6fvViReva0CFkIRppRIlQSKRJRatAAAAAAAAAAAAASaSKIRSiGQLJpRSOgDAAIA
+# Saved Loadout: Magmorax
+# talents = B4QAa92LNqySTL5b6fvViReva0CFkIRppRIlQSKRJRatAAAAAAAAAAAAASaSKIRSiWB0SSakkDAAgA
+# Saved Loadout: Echo
+# talents = B4QAa92LNqySTL5b6fvViReva0CFkIhimkIikkWUSahAAAAAAAAAAAAASaSKhEJJaFBtkkGkDAAgA
+
+# Life - Bound Cap(447)
+head =, id = 193400, gem_id = 192945, bonus_id = 8836 / 8840 / 8902 / 9405 / 9376 / 8913 / 8960 / 8790 / 9366 / 9413, crafted_stats = 36 / 49, crafting_quality = 5
+# Tuskarr Bone Necklace(441)
+neck =, id = 193666, gem_id = 192985 / 192952 / 192952, bonus_id = 41 / 9144 / 9334 / 1663 / 8767 / 9477 / 8782
+# Spines of the Vermillion Forge(447)
+shoulder =, id = 202504, bonus_id = 6652 / 9382 / 9227 / 1504
+# Drape of the Vermillion Forge(441)
+back =, id = 202501, enchant_id = 6598, bonus_id = 6652 / 41 / 9334 / 1498
+# Cuirass of the Vermillion Forge(441)
+chest =, id = 202509, enchant_id = 6619, bonus_id = 6652 / 9231 / 9409 / 9334 / 1492 / 8767
+# Dark Silk Shirt(1)
+shirt =, id = 4333
+# Court of Harvesters Tabard(30)
+tabard =, id = 179282
+# Bloodstench Skinguards(441)
+wrist =, id = 202594, enchant_id = 6580, bonus_id = 9409 / 41 / 9414 / 9226 / 9220 / 9334 / 1498 / 8767
+# Fists of the Vermillion Forge(441)
+hands =, id = 202507, bonus_id = 6652 / 9230 / 9409 / 9334 / 1492 / 8767
+# Life - Bound Belt(447)
+waist =, id = 193407, enchant_id = 6904, gem_id = 192952, bonus_id = 8836 / 8840 / 8902 / 9405 / 9376 / 8790 / 9366 / 9413, crafted_stats = 36 / 40, crafting_quality = 5
+# Pantaloons of the Vermillion Forge(441)
+legs =, id = 202505, enchant_id = 6544, bonus_id = 6652 / 9228 / 9409 / 9334 / 1498 / 8767
+# Life - Bound Boots(447)
+feet =, id = 193398, enchant_id = 6613, bonus_id = 8836 / 8840 / 8902 / 9405 / 9376 / 8790 / 9366, crafted_stats = 40 / 32, crafting_quality = 5
+# Seal of Questionable Loyalties(437)
+finger1 =, id = 158314, enchant_id = 6556, gem_id = 192952, bonus_id = 6652 / 9413 / 9144 / 9329 / 3307 / 8767
+# Signet of Titanic Insight(447)
+finger2 =, id = 192999, enchant_id = 6556, gem_id = 192952, bonus_id = 8836 / 8840 / 8902 / 8780 / 9405 / 8790 / 9376 / 8174 / 9366, crafted_stats = 40 / 36, crafting_quality = 5
+# Screaming Black Dragonscale(447)
+trinket1 =, id = 202612, bonus_id = 6652 / 9410 / 9382 / 1498 / 8767
+# Rashok's Molten Heart (441)
+trinket2 =, id = 202614, bonus_id = 6652 / 9409 / 9334 / 1495 / 8767
+# Obsidian Seared Hexsword(447)
+main_hand =, id = 190511, enchant_id = 6643, bonus_id = 8836 / 8840 / 8902 / 9405 / 9376 / 8790 / 8913 / 8960 / 9366, crafted_stats = 49 / 32, crafting_quality = 5
+# Thadrion's Erratic Arcanotrode (447)
+off_hand =, id = 204318, bonus_id = 6652 / 9410 / 9382 / 1501 / 8767
+
+### Gear from Bags
+#
+# Mist - Piercing Goggles(30)
+# head =, id = 87213
+#
+# Elemental Lariat(421)
+# neck =, id = 193001, gem_id = 192991 / 192932 / 192932, bonus_id = 8836 / 8840 / 8902 / 8960 / 8783 / 8782 / 9405 / 8795 / 8846 / 9365, crafted_stats = 32 / 40, crafting_quality = 5
+#
+# Eye of the Vengeful Hurricane(421)
+# neck =, id = 195496, gem_id = 192985 / 192945 / 192945, bonus_id = 6652 / 7981 / 1498 / 8767 / 8784 / 8782
+#
+# Bromach's Disentombed Locket (428)
+# neck =, id = 193809, bonus_id = 9330 / 6652 / 9415 / 8783 / 9144 / 1650 / 8767
+#
+# Spines of the Vermillion Forge(437)
+# shoulder =, id = 202504, bonus_id = 6652 / 9227 / 7979 / 9329 / 1494 / 8767
+#
+# Gravelworn Handguards(431)
+# hands =, id = 134443, bonus_id = 9331 / 6652 / 9223 / 9218 / 9144 / 3310 / 8767
+#
+# Blightweaver's Clutches (431)
+# hands =, id = 193661, bonus_id = 9331 / 6652 / 9223 / 9218 / 9144 / 1653 / 8767
+#
+# Flamebound Huntsman's Footpads (441)
+# feet =, id = 202574, enchant_id = 6613, bonus_id = 9409 / 6652 / 9226 / 9219 / 9334 / 1498 / 8767
+#
+# Onyx Annulet(424)
+# finger1 =, id = 203460, enchant_id = 6556, gem_id = 204010 / 204013 / 204020, bonus_id = 1491, gem_bonus_id = 1472 / 2 / 9369 / 1472 / 2 / 9369 / 1472 / 0 / 0
+#
+# Loop of Pulsing Veins(437)
+# finger1 =, id = 159463, enchant_id = 6556, bonus_id = 41 / 9414 / 9144 / 9329 / 3307 / 8767
+#
+# Onyx Annulet(424)
+# finger1 =, id = 203460, enchant_id = 6550, gem_id = 204000 / 204027 / 204022, bonus_id = 1491, gem_bonus_id = 1472 / 2 / 9369 / 1472 / 2 / 9369 / 1472 / 0 / 0
+#
+# Unstable Arcane Loop(415)
+# finger1 =, id = 193633, enchant_id = 6556, bonus_id = 8973 / 7977 / 6652 / 7936 / 9144 / 1637 / 8767
+#
+# Thunderous Downburst Ring(402)
+# finger1 =, id = 193696, enchant_id = 6556, bonus_id = 8969 / 7977 / 6652 / 7937 / 9144 / 1624 / 8767
+#
+# Circle of Ascended Frost(441)
+# finger1 =, id = 193731, enchant_id = 6556, bonus_id = 6652 / 9415 / 9144 / 9334 / 1663 / 8767
+#
+# Band of the Wyrm Matron(447)
+# finger1 =, id = 134524, enchant_id = 6556, bonus_id = 9382 / 6652 / 9414 / 9144 / 3326 / 8767
+#
+# Captain's Signet of Command (70)
+# finger1 =, id = 166560
+#
+# Seal of Questionable Loyalties(441)
+# finger1 =, id = 158314, bonus_id = 40 / 9415 / 9144 / 9334 / 3311 / 8767
+#
+# Ring of Frozen Rain(424)
+# finger1 =, id = 206185, enchant_id = 6550, bonus_id = 6652 / 9415 / 9144 / 9324 / 1646 / 8767
+#
+# Globe of Jagged Ice(441)
+# trinket1 =, id = 193732, bonus_id = 6652 / 9144 / 9334 / 1663 / 8767
+#
+# Irideus Fragment(428)
+# trinket1 =, id = 193743, bonus_id = 9330 / 6652 / 9144 / 1650 / 8767
+#
+# Decoration of Flame(415)
+# trinket1 =, id = 194299, bonus_id = 7981 / 6652 / 1498 / 8767
+#
+# Idol of the Dreamer(421)
+# trinket1 =, id = 193005, bonus_id = 8836 / 8840 / 8902 / 9405 / 8846 / 9365, crafting_quality = 5
+#
+# Broodkeeper's Promise (424)
+# trinket1 =, id = 194307, bonus_id = 7981 / 41 / 1498 / 8767
+#
+# Alacritous Alchemist Stone(421)
+# trinket1 =, id = 191492, bonus_id = 8836 / 8840 / 8902 / 9405 / 8846 / 9365, crafting_quality = 5
+#
+# Whispering Incarnate Icon(421)
+# trinket1 =, id = 194301, bonus_id = 6652 / 7981 / 1498 / 8767
+#
+# Dragonscale Ripper(441)
+# main_hand =, id = 193772, enchant_id = 6643, bonus_id = 6652 / 9147 / 9334 / 1663 / 8767
+#
+# Claws of the Blazing Behemoth(441)
+# main_hand =, id = 202560, enchant_id = 6642, bonus_id = 6652 / 9409 / 9334 / 1492 / 8767
+#
+# Wirt's Last Leg (1)
+# main_hand =, id = 206276
+#
+# Crackling Codex of the Isles(421)
+# off_hand =, id = 194879, bonus_id = 8836 / 8840 / 8902 / 9405 / 8790 / 8846 / 9365, crafted_stats = 32 / 40, crafting_quality = 5
+
+### Additional Character Info
+#
+# upgrade_currencies = c: 1792: 627 / c: 2122: 17 / c: 2245: 2000 / i: 204195: 21 / i: 198046: 1 / i: 204440: 1 / i: 204193: 20 / i: 204697: 1 / i: 190453: 1 / i: 204682: 1 / i: 198058: 1 / i: 197921: 5 / i: 198056: 1 / i: 198048: 2 / i: 204196: 14
+#
+# slot_high_watermarks = 0: 447: 447 / 1: 441: 441 / 2: 447: 447 / 3: 441: 441 / 4: 447: 447 / 5: 441: 441 / 6: 447: 447 / 7: 441: 441 / 8: 441: 441 / 9: 447: 447 / 10: 441: 441 / 11: 441: 441 / 12: 437: 437 / 13: 447: 447 / 14: 441: 441 / 15: 441: 441 / 16: 447: 447
+
+# Checksum: 69ee90ed
+**/
+
